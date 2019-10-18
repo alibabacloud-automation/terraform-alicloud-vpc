aefb645e4c5ca82808eb34009905477173ddb675
LexWong <914088735@qq.com>
2019-10-18 17:50:13 +0800

    improve(vpc): modified the vpc module outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
 // Output the IDs of the ECS instances created
 output "vpc_id" {
-  value = var.vpc_id == "" ? join(",", alicloud_vpc.vpc.*.id) : var.vpc_id
+  value = var.vpc_id == "" ? alicloud_vpc.vpc.*.id[0] : var.vpc_id
 }
 
 output "vswitch_ids" {
-  value = join(",", alicloud_vswitch.vswitches.*.id)
+  value = alicloud_vswitch.vswitches.*.id
 }
 
 output "availability_zones" {
-  value = join(",", alicloud_vswitch.vswitches.*.availability_zone)
+  value = alicloud_vswitch.vswitches.*.availability_zone
 }
 
 output "router_id" {
-  value = join(",",alicloud_route_entry.route_entry.*.router_id)
+  value = alicloud_route_entry.route_entry.*.router_id
 }
 
 output "route_table_id" {
-  value = join(",",alicloud_route_entry.route_entry.*.route_table_id)
+  value = alicloud_route_entry.route_entry.*.route_table_id
 }
