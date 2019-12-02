076d42c86ef72f3c04799dc6fb2fe1dfc18c4bd7
He Guimin <guimin.hgm@alibaba-inc.com>
2019-12-02 20:33:24 +0800

    Fix count.index error

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+## 1.5.0 (Unreleased)
+## 1.4.0 (December 2, 2019)
+
+IMPROVEMENTS:
+
+- support retrieving the existing VPC by name regex and tags automatically [GH-15](https://github.com/terraform-alicloud-modules/terraform-alicloud-vpc/pull/15)
+
+BUG FIXES:
+
+- Fix count.index error [GH-15](https://github.com/terraform-alicloud-modules/terraform-alicloud-vpc/pull/15)
+
+## 1.3.1 (October 30, 2019)
+
+IMPROVEMENTS:
+
+- Support output cidr_blocks
+
+
--- main.tf
+++ main.tf
@@ -7,21 +7,14 @@ provider "alicloud" {
   configuration_source    = "terraform-alicloud-modules/vpc"
 }
 
-// Instance_types data source for instance_type
-data "alicloud_instance_types" "default" {
-  cpu_core_count = var.cpu_core_count
-  memory_size    = var.memory_size
-}
-
 // Zones data source for availability_zone
 data "alicloud_zones" "default" {
   available_resource_creation = "VSwitch"
-  available_instance_type     = data.alicloud_instance_types.default.instance_types[0].id
 }
 
 // If there is not specifying vpc_id, the module will launch a new vpc
 resource "alicloud_vpc" "vpc" {
-  count       = var.vpc_id == "" ? 1 : 0
+  count       = local.vpc_id == "" ? 1 : 0
   name        = var.vpc_name
   cidr_block  = var.vpc_cidr
   description = var.vpc_description
@@ -32,7 +25,7 @@ resource "alicloud_vswitch" "vswitches" {
   count             = length(var.vswitch_cidrs)
   vpc_id            = var.vpc_id != "" ? var.vpc_id : alicloud_vpc.vpc[0].id
   cidr_block        = var.vswitch_cidrs[count.index]
-  availability_zone = var.availability_zones[count.index] != "" ? var.availability_zones[count.index] : lookup(data.alicloud_zones.default.zones[format("%d", length(data.alicloud_zones.default.zones) < 2 ? 0 : count.index % length(data.alicloud_zones.default.zones))], "id")
+  availability_zone = length(var.availability_zones) > 0 ? var.availability_zones[count.index] : element(data.alicloud_zones.default.ids.*, count.index)
   name              = length(var.vswitch_cidrs) < 2 ? var.vswitch_name : format("%s_%s", var.vswitch_name, format(var.number_format, count.index + 1))
   description       = length(var.vswitch_cidrs) < 2 ? var.vswitch_description : format("%s This is NO.%s", var.vswitch_description, format(var.number_format, count.index + 1))
 }
@@ -40,7 +33,7 @@ resource "alicloud_vswitch" "vswitches" {
 // According to the destination cidr block to launch a new route entry
 resource "alicloud_route_entry" "route_entry" {
   count                 = length(var.destination_cidrs)
-  route_table_id        = var.route_table_id != "" ? var.route_table_id : var.vpc_id == "" ? alicloud_vpc.vpc[0].route_table_id : ""
+  route_table_id        = local.route_table_id
   destination_cidrblock = var.destination_cidrs[count.index]
   nexthop_type          = "Instance"
   nexthop_id            = var.nexthop_ids[count.index]
