3098fd531fa4a0210783f4d04bb6e2d748d7964a
Brian BORDINI <brian.bordini@richemont.com>
2020-08-13 16:58:57 +0200

    redevelop the VPC module to include NAT GW, CEN and upgrade to Terraform 0.12 constructs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,8 @@
 // Output the IDs of the ECS instances created
+
 output "vpc_id" {
-  description = "Deprecated and use this_vpc_id instead"
-  value       = local.this_vpc_id
+  description = "The VPC ID"
+  value       = local.vpc_id
 }
 
 output "cidr_block" {
@@ -9,81 +10,54 @@ output "cidr_block" {
   value       = concat(alicloud_vpc.vpc.*.cidr_block, [""])[0]
 }
 
-output "vswitch_ids" {
-  description = "Deprecated and use this_vswitch_ids instead"
-  value       = alicloud_vswitch.vswitches.*.id
-}
-
-output "availability_zones" {
-  description = "Deprecated and use this_availability_zones instead"
-  value       = alicloud_vswitch.vswitches.*.availability_zone
-}
-
-output "router_id" {
-  description = "Deprecated and use this_router_id instead"
-  value       = alicloud_route_entry.route_entry.*.router_id
-}
-
-output "route_table_id" {
-  description = "Deprecated and use this_route_table_id instead"
-  value       = alicloud_route_entry.route_entry.*.route_table_id
-}
-
-output "this_vpc_id" {
-  description = "The VPC id"
-  value       = local.this_vpc_id
-}
-
-output "this_vpc_name" {
+output "vpc_name" {
   description = "The VPC name"
-  value       = local.this_vpc_name
+  value       = concat(alicloud_vpc.vpc.*.name, [""])[0]
 }
 
-output "this_vpc_cidr_block" {
-  description = "The VPC cidr block"
-  value       = local.this_vpc_cidr_block
-}
-
-output "this_vpc_tags" {
+output "vpc_tags" {
   description = "The VPC tags"
   value       = concat(alicloud_vpc.vpc.*.tags, [{}])[0]
 }
 
-output "this_resource_group_id" {
+output "resource_group_id" {
   description = "The Id of resource group which the instance belongs."
   value       = concat(alicloud_vpc.vpc.*.resource_group_id, [])[0]
 }
 
-output "this_vswitch_ids" {
+output "nat_gateway_id" {
+  description = "The Id of resource group which the instance belongs."
+  value       = concat(alicloud_nat_gateway.default.*.id, [])[0]
+}
+
+output "nat_gateway_snat_table_ids" {
+  description = "The Id of resource group which the instance belongs."
+  value       = concat(alicloud_nat_gateway.default.*.snat_table_ids, [])[0]
+}
+
+output "vswitches_ids" {
   description = "List of vswitch ids"
-  value       = alicloud_vswitch.vswitches.*.id
+  value       = [for value in alicloud_vswitch.vswitch : value.id]
 }
 
-output "this_vswitch_names" {
+/* convert in complex type ?
+output "vswitch_names" {
   description = "List of vswitch names"
-  value       = alicloud_vswitch.vswitches.*.name
+  value       = [for value in alicloud_vswitch.vswitch: value.name]
 }
 
-output "this_vswitch_cidr_blocks" {
+output "vswitch_cidr_blocks" {
   description = "The vswitch cidr block"
-  value       = alicloud_vswitch.vswitches.*.cidr_block
+  value       = [for value in alicloud_vswitch.vswitch: value.cidr_block]
 }
 
-output "this_vswitch_tags" {
+output "vswitch_tags" {
   description = "List of vswitch tags."
-  value       = alicloud_vswitch.vswitches.*.tags
+  value       = [for value in alicloud_vswitch.vswitch: value.tags]
 }
 
 output "this_availability_zones" {
   description = "List of availability zones in which vswitches launched."
   value       = alicloud_vswitch.vswitches.*.availability_zone
 }
-
-output "this_route_table_id" {
-  description = "The vpc route table id."
-  value       = local.route_table_id
-}
-output "this_router_id" {
-  description = "The vpc router id."
-  value       = concat(alicloud_route_entry.route_entry.*.router_id, [""])[0]
-}
+*/
--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+# Cloud Project variable
+
+variable "project_name" {
+  description = "The project name"
+  type        = string
+  default     = ""
+}
+
+# Terraform general variables
 variable "region" {
   description = "The region used to launch this module resources."
   type        = string
@@ -22,16 +31,10 @@ variable "skip_region_validation" {
 }
 
 # VPC variables
-variable "create" {
-  description = "Whether to create vpc. If false, you can specify an existing vpc by setting 'vpc_id'."
+variable "create_vpc" {
+  description = "Whether to create vpc. If false, you must specify an existing vpc by setting 'vpc_id'."
   type        = bool
   default     = true
-}
-
-variable "vpc_id" {
-  description = "The vpc id used to launch several vswitches. If set, the 'create' will be ignored."
-  type        = string
-  default     = ""
 }
 
 variable "vpc_name" {
@@ -52,70 +55,85 @@ variable "vpc_cidr" {
   default     = "172.16.0.0/12"
 }
 
+variable "vpc_id" {
+  description = "The VPC ID when it is already created."
+  type        = string
+  default     = ""
+}
+
 variable "resource_group_id" {
   description = "The Id of resource group which the instance belongs."
   type        = string
   default     = ""
 }
 
-variable "vpc_name_regex" {
-  description = "(Deprecated) It has been deprecated from 1.5.0."
-  type        = string
-  default     = ""
+variable "tags" {
+  description = "The tags assigned to the whole module."
+  type        = map(string)
+  default     = {}
 }
 
 variable "vpc_tags" {
-  description = "The tags used to launch a new vpc. Before 1.5.0, it used to retrieve existing VPC."
+  description = "The tags assigned to the vpc."
   type        = map(string)
   default     = {}
 }
 
 # VSwitch variables
-variable "vswitch_cidrs" {
-  description = "List of cidr blocks used to launch several new vswitches. If not set, there is no new vswitches will be created."
-  type        = list(string)
-  default     = []
+# TODO
+variable "vswitches" {
+  description = "A list of the specific CIDR blocks desired for each public subnet. The key must be in the form AZ-0, AZ-1, ... AZ-n where n is the number of Availability Zones."
+  type        = map(object({ name = string, cidr_block = string, availability_zone = string, description = string }))
+  default     = {}
 }
 
-variable "availability_zones" {
-  description = "List available zones to launch several VSwitches."
-  type        = list(string)
-  default     = []
-}
-
-variable "vswitch_name" {
-  description = "The vswitch name prefix used to launch several new vswitches."
-  default     = "TF-VSwitch"
-}
-
-variable "use_num_suffix" {
-  description = "Always append numerical suffix(like 001, 002 and so on) to vswitch name, even if the length of `vswitch_cidrs` is 1"
-  type        = bool
-  default     = false
-}
-
-variable "vswitch_description" {
-  description = "The vswitch description used to launch several new vswitch."
-  type        = string
-  default     = "New VSwitch created by Terrafrom module terraform-alicloud-vpc."
-}
-
-variable "vswitch_tags" {
-  description = "The tags used to launch serveral vswitches."
+variable "vswitches_tags" {
+  description = "The tags assigned only to vswitches"
   type        = map(string)
   default     = {}
 }
 
-// According to the vswitch cidr blocks to launch several vswitches
-variable "destination_cidrs" {
-  description = "List of destination CIDR block of virtual router in the specified VPC."
-  type        = list(string)
-  default     = []
+# NAT Gateway variables
+variable "create_nat_gateway" {
+  type    = bool
+  default = false
 }
 
-variable "nexthop_ids" {
-  description = "List of next hop instance IDs of virtual router in the specified VPC."
-  type        = list(string)
-  default     = []
+variable "nat_gateway_specification" {
+  default = "Small"
 }
 
+variable "nat_eip_bandwidth" {
+  default = 1
+}
+
+variable "nat_gateway_num_eips" {
+  default = 1
+}
+
+# Routes variables
+variable "custom_routes" {
+  description = "A list of the specific routes desired for the route table."
+  type        = map(object({ destination_cidrblock = string, nexthop_type = string, nexthop_id = string }))
+  default     = {}
+}
+
+
+# CEN
+variable "cen_enabled" {
+  description = "The VPC must or must not access to the CEN"
+  type        = bool
+  default     = false
+}
+
+variable "cen_id" {
+  description = "The CEN identifier"
+  type        = string
+  default     = null
+}
+
+variable "cen_owner_id" {
+  description = "The CEN owner identifier"
+  type        = string
+  default     = null
+}
