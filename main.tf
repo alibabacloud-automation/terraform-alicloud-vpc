52f8b66a9f486f3d3a2d681bd6c854736dd8d8be
Jose Vicente Blasco Torres <jvblasco@sebafactory.com>
2021-08-17 09:27:07 +0200

    Update attribute names to avoid deprecation warnings

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "alicloud" {
 // If there is not specifying vpc_id, the module will launch a new vpc
 resource "alicloud_vpc" "vpc" {
   count             = var.vpc_id != "" ? 0 : var.create ? 1 : 0
-  name              = var.vpc_name
+  vpc_name          = var.vpc_name
   cidr_block        = var.vpc_cidr
   resource_group_id = var.resource_group_id
   description       = var.vpc_description
@@ -26,8 +26,8 @@ resource "alicloud_vswitch" "vswitches" {
   count             = local.create_sub_resources ? length(var.vswitch_cidrs) : 0
   vpc_id            = var.vpc_id != "" ? var.vpc_id : concat(alicloud_vpc.vpc.*.id, [""])[0]
   cidr_block        = var.vswitch_cidrs[count.index]
-  availability_zone = element(var.availability_zones, count.index)
-  name              = length(var.vswitch_cidrs) > 1 || var.use_num_suffix ? format("%s%03d", var.vswitch_name, count.index + 1) : var.vswitch_name
+  zone_id           = element(var.availability_zones, count.index)
+  vswitch_name      = length(var.vswitch_cidrs) > 1 || var.use_num_suffix ? format("%s%03d", var.vswitch_name, count.index + 1) : var.vswitch_name
   description       = var.vswitch_description
   tags = merge(
     {
