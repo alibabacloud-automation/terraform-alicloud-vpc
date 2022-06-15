bc80a006ff3466832e79fb27de3899b59bb1b23e
MrWolong <709503583@qq.com>
2022-06-15 15:21:51 +0800

    test github actions

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,7 @@
-if [ ! -d "exampl" ]; then
+if [ ! -d "examples" ]; then
   echo "FAIL: Please Add the EXAMPLES"
+fi
+
+if [ ! -d "examples\complete" ]; then
+  echo "FAIL: Please Add the complete"
 fi
